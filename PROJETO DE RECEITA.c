2ea8e4ff0526f0c0a843ac8b2d003673177d7eae
CaioEdur <claudecirtatu2017@gmail.com>
2026-09-17 20:34:52 -0300

    Refactor recipe program to include bread ingredients

--- PROJETO DE RECEITA.c
+++ PROJETO DE RECEITA.c
@@ -4,22 +4,56 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void re1 (){
-		printf("NOME DA RECEITA");
 		
-		int primeiraresposta, ing1, ing2, falta;
+	printf("=======PÃO========");
 		
-		printf("\nIngredientes: ");
-		printf("\n.....");
-		printf("\n.....");
+	int primeiraresposta, falta1 , falta2 , falta3 , combo;
 		
-		printf("\nExiste algun ingrediente que voce nao possua |1 - sim| |2 - nao| : ");
-		scanf("%d", &primeiraresposta);
+	printf("\nIngredientes: ");
+	
+	printf("\n1 - 500g de farinha de trigo");
+	printf("\n2 - 10g de fermento biologico seco");
+	printf("\n3 - 10g de sal");
+	printf("\n4 - 10g de acucar");
+	printf("\n5 - 300ml de leite");
+	printf("\n6 - 20ml de margarina");
+	printf("\nBonus caso queira - 1 colher de cha de melhorador de farinha");
 		
-		if (primeiraresposta == 1){
-			printf("\nQual(is) ingredientes estao faltando (aponte pela numeracao de cada um) : ");
-			scanf("%d", &falta);
-			
-				printf("\n entao use isso no lugar");
+	printf("\nCaso voce precise substituir alguns, temos alguns ingredientes que podem\n ser trocados, e o seu caso |1 - sim| |2 - nao| : ");
+	scanf("%d", &primeiraresposta);
+		
+		switch (primeiraresposta){
+			case 1: {
+					printf("\nINGREDIENTES SUBSTITUIVEIS\n");
+					
+					printf("Possui acucar |1-SIM| |0-NAO|:  ");
+					scanf("%d", &falta1);
+					printf("\nPossui oleo |1-SIM| |0-NAO|:  ");
+					scanf("%d", &falta2);
+					printf("\nPossui leite |1-SIM| |0-NAO|:  ");
+					scanf("%d", &falta3);	
+									
+					combo = falta1 + falta2 + falta3;
+					
+					switch (combo){
+						case 1:{
+								printf("\nAçúcar ---> pode retirar ou use uma colher de chá de mel.");
+							break;
+						}
+						case 2:{
+							 	printf("Açúcar ---> pode retirar ou use uma colher de chá de mel.");
+							break;
+						}
+						
+					}
+						
+									
+				break;
+			}
+			case 3: {
+				
+				break;
+			}
 		}
 		
 }
@@ -49,7 +83,7 @@ int main(int argc, char *argv[]) {
 	
 	int op;
 	
-	printf("\nRECEITA");
+	printf("\nRECEITA: ");
 	scanf("%d", &op);
 	
 	switch (op){
